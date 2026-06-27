82b70151f887d345de630197f26b29b00e491acd
Kornel Kiszczuk <kornelkiszczuk@users.noreply.github.com>
2026-06-27 14:05:00 +0200

    Poprawki do Lintera

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,18 +1,20 @@
+from flask import request
+
 from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
-from flask import request
 
 moje_imie = "Kornel"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    return get_formatted(msg, moje_imie, output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
